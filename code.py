7618bee11091dbc7a1acf43554856305c0b9ca27
nikolaspaci <nikolaspaci>
2019-11-30 14:14:19 +0100

    jdd consommateur

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import cross_val_score
 
-""" Calcul de la moyenne d'une liste"""
+""" Calcul de la moyenne"""
 def moyenne(x):
     moy=[]
     xmeans=x.mean()
@@ -16,19 +16,18 @@ def moyenne(x):
         moy.append([mean])
     return numpy.array(moy)
 
-
-""" Calcul somme covariances des classes"""
+""" Calcul de la somme des covariances des classes"""
 def Sommecovariance(X,Y):
     z=(((len(X)*X)+(len(Y)*Y))/(len(X)+len(Y)))
     return z
 
-"""Adl"""
+"""Calcul de la règle de décision de l'Adl"""
 def RegleDeciAdl(point,covarianceInv,moy,pi):
     mulcovmoy=numpy.dot(covarianceInv,moy)
     Rd=(numpy.dot(numpy.transpose(point),mulcovmoy)-(1/2)*numpy.dot(numpy.transpose(moy),mulcovmoy)+numpy.log(pi))
     return Rd
 
-"""Tracer de la frontière de décision"""
+"""Tracer de la frontière de décision de l'ADL"""
 def tracerFrontiereDecision(moyobs0,moyobs1,pi0,pi1,covarianceInv):
     b=numpy.transpose(moyobs0-moyobs1)
     b=numpy.dot(b,covarianceInv)
@@ -48,7 +47,7 @@ def tracerFrontiereDecision(moyobs0,moyobs1,pi0,pi1,covarianceInv):
     z.append(x1fd[0][0])
     plt.plot(z,p,label='Frontiere décision ADL réalisé',color='green')
 
-"""ADL"""
+"""Estimation des parametrès pour appliquer l'ADL"""
 def estimerParamAdl(obs0,obs1):
     moyobs0=moyenne(obs0)
     moyobs1=moyenne(obs1)
@@ -62,23 +61,25 @@ def estimerParamAdl(obs0,obs1):
     covarianceInv=numpy.linalg.pinv(covSomme)
     return moyobs0,moyobs1,pi0,pi1,covarianceInv
 
+"""Séparation des observations du dataframe"""
 def sepObs(WS):
     obs0=WS.loc[WS['y']==0.0,['X1','X2']]
     obs1=WS.loc[WS['y']==1.0,['X1','X2']]
     return obs0,obs1
 
+"""Fonction de prédiction pour un modèle ADL"""
 def predictLDA(point):
     Rd0=RegleDeciAdl(point,covarianceInv,moyobs0,pi0)
     Rd1=RegleDeciAdl(point,covarianceInv,moyobs1,pi1)
-    print("R0 pour ce point: ",Rd0)
-    print("R1 pour ce point: ",Rd1)
+    print("S0 de notre ADL pour Xtest: ",Rd0)
+    print("S1 de notre ADL pour Xtest: ",Rd1)
     if Rd0>Rd1:
-        print("On prédit la classe 0")
+        print("Notre ADL prédit la classe 0")
     else:
-        print("on prédit la classe 1")
+        print("Notre ADL prédit la classe 1")
 
-"""Validation croisée LOO"""
-def validationCroisé(obs0,obs1):
+"""Validation croisée Leave One Out"""
+def validationCroisée(obs0,obs1):
     nbObservation=len(obs0)+len(obs1)
     tn=0
     tp=0
@@ -96,10 +97,11 @@ def validationCroisé(obs0,obs1):
         if RegleDeciAdl(point,covarianceInv,moyobs0,pi0)<RegleDeciAdl(point,covarianceInv,moyobs1,pi1) :
             tp+=1
 
-    return (tp+tn)/nbObservation
+    return ((tp+tn)/nbObservation)*100
 
+"""Fonction en charge de la prédiction d'un point pour ADL de SkLearn"""
 def sklLDAPredict(X,Y,point):
-    """prediction"""
+    """Prédiction du point"""
     clf = LinearDiscriminantAnalysis()
     clf.fit(X,Y)
     LinearDiscriminantAnalysis(n_components=None, priors=None, shrinkage=None, solver='svd',
@@ -107,52 +109,31 @@ def sklLDAPredict(X,Y,point):
     w = clf.coef_[0]
     a = -w[0] / w[1]
     yy = a * X - (clf.intercept_[0]) / w[1]
-    print("Lda de sklearn predit la classe : ",clf.predict(point))
+    print("Lda de sklearn prédit la classe : ",clf.predict(point),' pour Xtest')
     plt.plot(X, yy, label='Frontiere décision ADL sklearn',color='orange')
-    """Validation croisée"""
-    scoreldaskl=cross_val_score(clf,X,Y,cv=LeaveOneOut())
-    print('Mean Accuracy lda skl',sum(scoreldaskl)/len(scoreldaskl))
-    #print(scoreldaskl)
 
-
+"""Fonction en charge de la prédiction d'un point pour la classifcation Logistique de SkLearn"""
 def sklLogisticPredict(X,Y,point):
+    """Prédiction du point"""
     clflogis = LogisticRegression(random_state=0,solver='liblinear')
     clflogis.fit(X,Y)
     w = clflogis.coef_[0]
     a = -w[0] / w[1]
     yy = a * X - (clflogis.intercept_[0]) / w[1]
-    plt.plot(X, yy,label='Frontiere décision logistique sklearn',color='blue')
-    print("logistique de sklearn predit la classe : ",clflogis.predict(point))
+    plt.plot(X, yy,label='Frontiere de décision de classification logistique sklearn',color='blue')
+    print("Classification logistique de sklearn prédit la classe : ",clflogis.predict(point),' pour Xtest')
+
+"""Fonction en charge de la validation croiséé pour la classifcation ADL de SkLearn"""
+def validationCroiséeSklLDA(X,Y):
+    clf = LinearDiscriminantAnalysis()
+    scoreldaskl=cross_val_score(clf,X,Y,cv=LeaveOneOut())
+    print('Accuracy de ADL de Sklearn : ',(sum(scoreldaskl)/len(scoreldaskl))*100,'%')
+
+"""Fonction en charge de la validation croiséé pour la classifcation Logistique de SkLearn"""
+def validationCroiséeSklLOG(X,Y):
+    clflogis = LogisticRegression(random_state=0,solver='liblinear')
     scorelogisskl=cross_val_score(clflogis,X,Y,cv=LeaveOneOut())
-    print('Mean accuracy logistique skl',sum(scorelogisskl)/len(scorelogisskl))
-
-
-####Analyse d'un set de données
-print("Jeu de donées")
-WS = pd.read_csv('online_shoppers_intention.csv',',')
-#Nettoyage des jeux de donées
-onehot = pd.get_dummies(WS['Month'])
-WS = WS.drop('Month',axis = 1)
-WS = WS.join(onehot)
-
-onehot = pd.get_dummies(WS['VisitorType'])
-WS = WS.drop('VisitorType',axis = 1)
-WS = WS.join(onehot)
-WS['Weekend']=WS['Weekend'].map({True: 1, False: 0})
-
-#print("NAN VALUE",WS.isnull().any())
-WS=WS.dropna() #supp nul
-obs0=WS.loc[WS['Revenue']==True,WS.columns!= 'Revenue']
-obs1=WS.loc[WS['Revenue']==False,WS.columns!= 'Revenue']
-#y=WS['Revenue']
-pi0=len(obs0)/(len(WS))
-pi1=len(obs1)/(len(WS))
-covObs0=numpy.cov(obs0.T)
-covObs1=numpy.cov(obs1.T)
-covSomme=Sommecovariance(covObs0,covObs1)
-covarianceInv=numpy.linalg.pinv(covSomme)
-moyobs0,moyobs1,pi0,pi1,covarianceInv=estimerParamAdl(obs0,obs1)
-print('Mean Accuracy Donnée', validationCroisé(obs0,obs1))
+    print('Accuracy de la classification logistique de SkLearn : ',(sum(scorelogisskl)/len(scorelogisskl))*100,'%')
 
 ####DATASET1
 ##1 Chargement des données et Nuage de points
@@ -163,26 +144,32 @@ plt.title("Nuage de points des données du DataSet1")
 plt.xlabel('x1')
 plt.ylabel('x2')
 plt.scatter(WS['X1'],WS['X2'],c=WS['y'])
-##2 ADL
+##2 Implémentation de l'ADL
 obs0,obs1=sepObs(WS)
 moyobs0,moyobs1,pi0,pi1,covarianceInv=estimerParamAdl(obs0,obs1)
-##3
-"""Frontiere decision"""
+##3 Frontière de décision de l'ADL et prédiction
+"""Frontière de décision"""
 tracerFrontiereDecision(moyobs0,moyobs1,pi0,pi1,covarianceInv)
-"""Prediction du point"""
+"""Prédiction du point"""
 pointpredictarray=numpy.array([-10,10])
 plt.scatter(pointpredictarray[0],pointpredictarray[1],c='r',label='point à prédire')
 predictLDA(pointpredictarray)
-##4
-#LDA CODE
-print('Mean Accuracy lda codé', validationCroisé(obs0,obs1))
-#LDA SKLEARN
+##4 Comparaison  des résultats avec les modèles de Sklearn
 X=WS[['X1','X2']]
 Y=WS['y']
 point=[[-10, 10]]
+#LDA SKLEARN
 sklLDAPredict(X,Y,point)
 #Logistique SKLEARN
 sklLogisticPredict(X,Y,point)
+###5 Comparaison des erreurs de classification
+#LDA CODE
+print('Accuracy de ADL codée', validationCroisée(obs0,obs1),'%')
+#LDA SKLEARN
+validationCroiséeSklLDA(X,Y)
+#Logistique SKLEARN
+validationCroiséeSklLOG(X,Y)
+plt.draw()
 
 ####DATASET2
 ##1 Chargement des données et Nuage de points
@@ -193,27 +180,32 @@ plt.title("Nuage de points des données du DataSet2")
 plt.xlabel('x1')
 plt.ylabel('x2')
 plt.scatter(WS['X1'],WS['X2'],c=WS['y'])
-##2 ADL
+##2 Implémentation de l'ADL
 obs0,obs1=sepObs(WS)
 moyobs0,moyobs1,pi0,pi1,covarianceInv=estimerParamAdl(obs0,obs1)
-##3
-"""Frontiere decision"""
+##3 Frontière de décision de l'ADL et prédiction
+"""Frontière de décision"""
 tracerFrontiereDecision(moyobs0,moyobs1,pi0,pi1,covarianceInv)
-"""Prediction du point"""
+"""Prédiction du point"""
 pointpredictarray=numpy.array([-10,10])
 plt.scatter(pointpredictarray[0],pointpredictarray[1],c='r',label='point à prédire')
 predictLDA(pointpredictarray)
-##4
-#LDA CODE
-print('Mean Accuracy lda codé', validationCroisé(obs0,obs1))
-#LDA SKLEARN
+##4 Comparaison  des résultats avec les modèles de Sklearn
 X=WS[['X1','X2']]
 Y=WS['y']
 point=[[-10, 10]]
+#LDA SKLEARN
 sklLDAPredict(X,Y,point)
 #Logistique SKLEARN
 sklLogisticPredict(X,Y,point)
-
+###5 Comparaison des erreurs de classification
+#LDA CODE
+print('Accuracy de ADL codée', validationCroisée(obs0,obs1),'%')
+#LDA SKLEARN
+validationCroiséeSklLDA(X,Y)
+#Logistique SKLEARN
+validationCroiséeSklLOG(X,Y)
+plt.draw()
 
 ####DATASET3
 ##1 Chargement des données et Nuage de points
@@ -224,25 +216,55 @@ plt.title("Nuage de points des données du DataSet3")
 plt.xlabel('x1')
 plt.ylabel('x2')
 plt.scatter(WS['X1'],WS['X2'],c=WS['y'])
-##2 ADL
+##2 Implémentation de l'ADL
 obs0,obs1=sepObs(WS)
 moyobs0,moyobs1,pi0,pi1,covarianceInv=estimerParamAdl(obs0,obs1)
-##3
-"""Frontiere decision"""
+##3 Frontière de décision de l'ADL et prédiction
+"""Frontière de décision"""
 tracerFrontiereDecision(moyobs0,moyobs1,pi0,pi1,covarianceInv)
-"""Prediction du point"""
+"""Prédiction du point"""
 pointpredictarray=numpy.array([-10,10])
 plt.scatter(pointpredictarray[0],pointpredictarray[1],c='r',label='point à prédire')
 predictLDA(pointpredictarray)
-##4
-#LDA CODE
-print('Mean Accuracy lda codé', validationCroisé(obs0,obs1))
-#LDA SKLEARN
+##4 Comparaison  des résultats avec les modèles de Sklearn
 X=WS[['X1','X2']]
 Y=WS['y']
 point=[[-10, 10]]
+#LDA SKLEARN
 sklLDAPredict(X,Y,point)
 #Logistique SKLEARN
 sklLogisticPredict(X,Y,point)
+###5 Comparaison des erreurs de classification
+#LDA CODE
+print('Accuracy de ADL codée', validationCroisée(obs0,obs1),'%')
+#LDA SKLEARN
+validationCroiséeSklLDA(X,Y)
+#Logistique SKLEARN
+validationCroiséeSklLOG(X,Y)
+plt.draw()
+
+####Analyse d'un jeu de données
+print("#######DataSet choisi#######")
+WS = pd.read_csv('online_shoppers_intention.csv',',')
+
+#Transformation des variables
+onehot = pd.get_dummies(WS['Month'])
+WS = WS.drop('Month',axis = 1)
+WS = WS.join(onehot)
+
+onehot = pd.get_dummies(WS['VisitorType'])
+WS = WS.drop('VisitorType',axis = 1)
+WS = WS.join(onehot)
+WS['Weekend']=WS['Weekend'].map({True: 1, False: 0})
+
+#Nettoyage du jeu de données
+WS=WS.dropna()
+
+obs0=WS.loc[WS['Revenue']==True,WS.columns!= 'Revenue']
+obs1=WS.loc[WS['Revenue']==False,WS.columns!= 'Revenue']
+
+#Application de l'ADL sur le jeu de données
+moyobs0,moyobs1,pi0,pi1,covarianceInv=estimerParamAdl(obs0,obs1)
+print('Accuracy du Jeu de donnée choisi', validationCroisée(obs0,obs1))
 
 plt.show()
